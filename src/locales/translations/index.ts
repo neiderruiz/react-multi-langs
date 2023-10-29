82b77f7a8af506058b5641dfdd76aa5ef16a6b65
Neider Ruiz <57574910+neiderruiz@users.noreply.github.com>
2023-10-29 10:55:34 -0500

    add translations

---
+++ src/locales/translations/index.ts
@@ -0,0 +1,16 @@
+import en from './en.json';
+import es from './es.json';
+import it from './it.json';
+const resources = {
+    es: {
+        translation: es
+    },
+    en: {
+        translation: en
+    },
+    it: {
+        translation: it
+    },
+}
+
+export default resources;
