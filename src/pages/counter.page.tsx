5825fa5342fa5a83d739d29946069870b915c1c8
Neider Ruiz <57574910+neiderruiz@users.noreply.github.com>
2023-10-29 08:26:45 -0500

    add base card

--- src/pages/counter.page.tsx
+++ src/pages/counter.page.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
 import Button from "../components/buttons/button";
 const MAX_COUNT = 10;
 const MIN_COUNT = 0;
@@ -42,6 +43,13 @@ const CounterPage = () => {
                     <span className="font-bold text-2xl">
                         Contando {counter} de {MAX_COUNT}
                     </span>
+                </div>
+                <div>
+                    <Link to="#" className="block  p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Bienvenidos a mi canal de youtube soy @neiderruiz</h5>
+                        <p className="font-normal text-gray-700 dark:text-gray-400">Desarrollador de software apasionado por la tecnología y el aprendizaje constante. Comparto información y consejos útiles sobre desarrollo.</p>
+                    </Link>
+
                 </div>
             </div>
         </div>
